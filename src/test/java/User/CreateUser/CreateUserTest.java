6b71f8e91cdd1dbcacd1fb4b043603757bc7df21
Nikita P <nporoykov@gmail.com>
2020-10-18 11:21:55 +0300

    1st

--- src/test/java/User/CreateUser/CreateUserTest.java
+++ src/test/java/User/CreateUser/CreateUserTest.java
@@ -11,14 +11,15 @@ import services.UserService;
 import static org.hamcrest.Matchers.*;
 
 
-public class CreateUserTest {
 
-    UserService userService = new UserService();
+public class CreateUserTest {
 
     @Test
     public void checkCreateUser() {
+        UserService userService = new UserService();
         Response response;
         User user;
+
         String expectedEmail = "Test@mail.ru";
         String actualType;
         String type = "unknown";
@@ -30,12 +31,12 @@ public class CreateUserTest {
 
         user = User.builder()
                 .email(expectedEmail)
-                .firstName("FirstName")
+                .firstName("1111")
                 .id(id)
                 .lastName("LastName")
                 .password("Password")
                 .phone("8-920-920-23-23")
-                .username("Ivan")
+                .username("1111")
                 .userStatus(10L)
                 .build();
 
@@ -52,4 +53,46 @@ public class CreateUserTest {
 
 
     }
+
+
+    @Test
+    public void checkCreateUser2() {
+        UserService userService = new UserService();
+        Response response1;
+        User user1;
+
+        String expectedEmail = "Test@mail.ru";
+        String actualType;
+        String type = "unknown";
+        String errorMessageType = "Incorrect userName";
+        String expectedType = "unknown";
+        Long id = 101L;
+
+        //  UserService userService = new UserService();
+
+        user1 = User.builder()
+                .email(expectedEmail)
+                .firstName("2222")
+                .id(id)
+                .lastName("LastName")
+                .password("Password")
+                .phone("8-920-920-23-23")
+                .username("2222")
+                .userStatus(10L)
+                .build();
+
+        response1 = userService.addUserRequest(user1);
+
+        response1
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.SC_OK)
+                .time(lessThan(5000L))
+                .body("type", equalTo(expectedType))
+                .body("message", comparesEqualTo( id.toString()));
+        // id.toString();
+
+
+    }
+
 }
